c6c85e9e2e3ae25b994fca48ac572211d273f247
Clement Teboul <teboul.clement@gmail.com>
2017-02-22 12:44:10 +0100

    Intégration page d'accueil

--- web/index.php
+++ web/index.php
@@ -2,12 +2,44 @@
 
 	require_once __DIR__.'/../vendor/autoload.php';
 
-	$app = new Silex\Application();
+	use Silex\Application;
+
+	const DB_HOST = 'localhost';
+	const DB_DATABASE = 'labyrintheSilex';
+	const USER = 'root';
+	const PASSWORD = '';
+
+	$app = new Application();
 
 	$app->register(new Silex\Provider\SessionServiceProvider());
 
-	$app->get('/hello/{name}', function($name) use($app) {
-	    return 'Hello '.$app->escape($name);
+	$app->register(new Silex\Provider\TwigServiceProvider(), array(
+	    'twig.path' => __DIR__.'/../views',
+	));
+
+	$app['debug'] = true;
+
+	$app['database.config'] = [
+	        'dsn'      => 'mysql:host=' . DB_HOST . ';dbname=' . DB_DATABASE,
+	        'username' => USER,
+	        'password' => PASSWORD,
+	        'options'  => [
+	                PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8", // flux en utf8
+	                PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,  // mysql erreurs remontées sous forme d'exception
+	                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ, // tous les fetch en objets
+	        ]
+	  
+	];
+
+	$app['pdo'] = function( $app ){
+		$options = $app['database.config'];
+
+		return new \PDO($options['dsn'],$options['username'],$options['password'],$options['options']);
+	};
+
+
+	$app->get('/', function() use($app) {
+	    return $app['twig']->render('front/home.twig');
 	});
 
 	$app->run();
